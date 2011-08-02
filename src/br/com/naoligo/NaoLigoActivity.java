eb33d35c0986bf3a95bcf7094e3d000829cfd7da
Emerson Barros <emersonbarros@gmail.com>
2011-08-01 21:14:05 -0300

    Correcao de bugs na selecao de contatos

--- src/br/com/naoligo/NaoLigoActivity.java
+++ src/br/com/naoligo/NaoLigoActivity.java
@@ -50,42 +50,35 @@ public class NaoLigoActivity extends Activity {
 				EditText editphone = (EditText) findViewById(R.id.editPhone);
 				EditText editmessage = (EditText) findViewById(R.id.editMessage);
 
+				String tmp = unformatPhone(editphone.getText().toString());
+				String ddd = tmp.substring(0, 2);
+				String phone = tmp.substring(2, tmp.length());
+
+				request.addProperty("email", email);
+				request.addProperty("senha", password);
+				request.addProperty("ddd", ddd);
+				request.addProperty("telefone", phone);
+				request.addProperty("texto", editmessage.getText().toString());
+
+				SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+						SoapEnvelope.VER11);
+
+				envelope.setOutputSoapObject(request);
+				HttpTransportSE androidHttpTransport = new HttpTransportSE(
+						Contants.URL);
+
 				try {
-					String tmp = formatPhone(editphone.getText().toString());
-					editphone.setText(tmp);
-					String ddd = tmp.substring(0, 2);
-					String phone = tmp.substring(2, tmp.length());
+					androidHttpTransport.call(Contants.SOAP_ACTION_SEND_SMS,
+							envelope);
 
-					request.addProperty("email", email);
-					request.addProperty("senha", password);
-					request.addProperty("ddd", ddd);
-					request.addProperty("telefone", phone);
-					request.addProperty("texto", editmessage.getText()
-							.toString());
+					String resultsRequestSOAP = (String) envelope.getResponse();
 
-					SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
-							SoapEnvelope.VER11);
-
-					envelope.setOutputSoapObject(request);
-					HttpTransportSE androidHttpTransport = new HttpTransportSE(
-							Contants.URL);
-
-					try {
-						androidHttpTransport.call(
-								Contants.SOAP_ACTION_SEND_SMS, envelope);
-
-						String resultsRequestSOAP = (String) envelope
-								.getResponse();
-
-						showDialog(resultsRequestSOAP);
-					} catch (Exception e) {
-						showDialog(e.getMessage());
-					} finally {
-						btSend.setText(getString(R.string.send));
-						btSend.setEnabled(true);
-					}
+					showDialog(resultsRequestSOAP);
 				} catch (Exception e) {
-					showDialog("O número do telefone deve estar no formato: ddd+telefone [1199990000]");
+					showDialog(e.getMessage());
+				} finally {
+					btSend.setText(getString(R.string.send));
+					btSend.setEnabled(true);
 				}
 			}
 		});
@@ -94,6 +87,8 @@ public class NaoLigoActivity extends Activity {
 			public void onClick(View v) {
 				Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
 						ContactsContract.Contacts.CONTENT_URI);
+				contactPickerIntent
+						.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
 				startActivityForResult(contactPickerIntent,
 						CONTACT_PICKER_RESULT);
 			}
@@ -115,47 +110,65 @@ public class NaoLigoActivity extends Activity {
 				Cursor cursor = managedQuery(intent.getData(), null, null,
 						null, null);
 				while (cursor.moveToNext()) {
-					String contactId = cursor.getString(cursor
-							.getColumnIndex(ContactsContract.Contacts._ID));
-
-					String hasPhone = cursor
+					String phoneNumber = cursor
 							.getString(cursor
-									.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+									.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-					if (hasPhone.equalsIgnoreCase("1")) {
-						Cursor phones = getContentResolver()
-								.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-										null,
-										ContactsContract.CommonDataKinds.Phone.CONTACT_ID
-												+ " = " + contactId, null, null);
-						if (phones.moveToNext()) {
-							String phoneNumber = phones
-									.getString(phones
-											.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-							EditText editphone = (EditText) findViewById(R.id.editPhone);
-							editphone.setText(formatPhone(phoneNumber));
-						}
-						phones.close();
-					}
+					EditText editphone = (EditText) findViewById(R.id.editPhone);
+					editphone.setText(formatPhone(phoneNumber));
+					break;
 				}
-				break;
+				cursor.close();
 			}
-
 		} else {
 			// gracefully handle failure
 			Log.w("DEBUG_TAG", "Warning: activity result not ok");
 		}
 	}
 
-	private String formatPhone(String phoneNumber) {
+	private String unformatPhone(String phoneNumber) {
 		if (phoneNumber != null) {
 			phoneNumber = phoneNumber.replaceAll("\\+55", "");
 			if (phoneNumber.startsWith("0")) {
 				phoneNumber = phoneNumber.replaceFirst("0", "");
 			}
-			if (phoneNumber.length() < 10) {
-				phoneNumber = "11" + phoneNumber;
+			phoneNumber = phoneNumber.replaceAll("-", "");
+			phoneNumber = phoneNumber.replaceAll("\\(", "");
+			phoneNumber = phoneNumber.replaceAll("\\)", "");
+			phoneNumber = phoneNumber.replaceAll(" ", "");
+			phoneNumber = phoneNumber.trim();
+		}
+		return phoneNumber;
+	}
+
+	private String formatPhone(String phoneNumber) {
+		if (phoneNumber != null) {
+			try {
+				phoneNumber = unformatPhone(phoneNumber);
+
+				String ddd = "11";
+				if (phoneNumber.length() == 13) {
+					ddd = phoneNumber.substring(4, 2);
+					phoneNumber = phoneNumber
+							.substring(6, phoneNumber.length());
+				} else if (phoneNumber.length() == 12) {
+					ddd = phoneNumber.substring(4, 2);
+					phoneNumber = phoneNumber
+							.substring(6, phoneNumber.length());
+				} else if (phoneNumber.length() == 10
+						|| phoneNumber.length() == 11) {
+					ddd = phoneNumber.substring(0, 2);
+					phoneNumber = phoneNumber
+							.substring(2, phoneNumber.length());
+				}
+
+				phoneNumber = phoneNumber
+						.substring(0, phoneNumber.length() - 4)
+						+ "-"
+						+ phoneNumber.substring(phoneNumber.length() - 4,
+								phoneNumber.length());
+				phoneNumber = "(" + ddd + ") " + phoneNumber;
+			} catch (Exception e) {
 			}
 		}
 		return phoneNumber;
